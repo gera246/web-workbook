fa5e97730ba2b0cafea5e10492b2d2175efbb063
Gerardo Rodriguez <MOLxGERA@Gerardos-MacBook-Air.local>
2017-06-08 17:31:00 -0500

    update

--- 06week/fizz-buzz/js/script.js
+++ 06week/fizz-buzz/js/script.js
@@ -9,58 +9,7 @@ Run!
 
 
 
-JavaScript
-
-document.getElementById("myTextBox").value
-document.getElementById("myButton").addEventListener("click", defaultFunction);
-
-function defaultFunction()
-{
-var number = 0;
-while (number < 100){
-number = number + 1;
- console.log(number);
-}
-
-}
-
-
-
-
-java with fizz
-
-document.getElementById("myTextBox").value
-document.getElementById("myButton").addEventListener("click", defaultFunction);
-
-function defaultFunction()
-{
-var number = 0;
-var fizz = number % 3;
-var buzz = number % 5;
-
-while (number < 100){
-number = number + 1;
-
-if(number % 3 != 0){
-	console.log(number);
-}
-else(
-	console.log("fizz"))
-
-if(number % 5 != 0){
-	console.log(number);
-}
-else(
-	console.log("buzz"))
-
-}
-
-}
-
-
-
-
-java with fizz and buzz
+java with fizz, buzz, and fizzbuzz (Correct!)
 
 
 document.getElementById("myTextBox").value
@@ -72,14 +21,19 @@ var number = 0;
 
 while (number < 100){
 number = number + 1;
+
 
 if(number % 3 != 0 && number % 5 != 0)
 {
 	console.log(number);
 }
-else
-{
-	if (number % 3 != 0)
+
+	if(number % 5 == 0 && number % 3 == 0)
+  {
+  console.log("fizzbuzz")
+  }
+  else{
+  if(number % 3 == 0)
   {
   console.log("fizz");
 	}
@@ -87,5 +41,7 @@ else
 	{
 	console.log("buzz");
 	}
+  }
+
 }
 }
